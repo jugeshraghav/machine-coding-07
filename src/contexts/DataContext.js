3f0d5d75e6b1e3c528523d6bac211415392a80f9
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2023-07-09 20:26:54 +0530

    fix: minor bug fix

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -3,21 +3,22 @@ import { createContext, useContext, useReducer } from "react";
 //initial state
 const initial_state = {};
 //reducers
-const reducer=(state,action)={
-    // const {type,payload}=action;
-    // switch(type){
-    //     default:
-    //         return state;
-    // }
-
-}
+const reducer = (state, action) => {
+  // const {type,payload}=action;
+  // switch(type){
+  //     default:
+  //         return state;
+  // }
+};
 const DataContext = createContext();
 
-const [state,dispatch]=useReducer(reducer,initial_state);
+const [state, dispatch] = useReducer(reducer, initial_state);
 const DataProvider = ({ children }) => {
-  <DataContext.Provider value={{state,dispatch}}>{children}</DataContext.Provider>;
+  <DataContext.Provider value={{ state, dispatch }}>
+    {children}
+  </DataContext.Provider>;
 };
 
-export const useData=()=>useContext(DataContext);
+export const useData = () => useContext(DataContext);
 
-export {DataProvider}
+export { DataProvider };
