3ff71a9128bd34d0213e76104e148edb1cfe68f1
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2023-07-09 22:23:57 +0530

    fix: ui fix

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -2,7 +2,7 @@ export const TravelCard = ({ image, name }) => {
   return (
     <>
       <div className="travel-card-container">
-        <img src={image} alt={name}></img>
+        <img src={image} alt={name} title={name}></img>
       </div>
     </>
   );
